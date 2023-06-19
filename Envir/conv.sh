45ece81c705cc9ac9be41bbc2d6cf95ed3bb5761
etorth <anhonghe@gmail.com>
2023-06-19 00:12:05 -0700

    f

--- Envir/conv.sh
+++ Envir/conv.sh
@@ -5,8 +5,20 @@ do
     lnk=`realpath $f`
     enc=`python guess_enc.py $f`
 
-    echo ${lnk} : ${enc}
-    iconv -f ${enc} -t utf-8 ${lnk} > ${lnk}.utf8
+    # echo ${lnk} : ${enc}
 
+    iconv -f ${enc} -t utf-8 ${lnk} > ${lnk}.utf8
+    if [ $? -ne 0 ]
+    then
+        if [ ${enc} == "GB2312" ]
+        then
+            iconv -f GB18030 -t utf-8 ${lnk} > ${lnk}.utf8
+            if [ $? -ne 0 ]
+            then
+                echo "Error: ${lnk} : ${enc}"
+                continue
+            fi
+        fi
+    fi
     mv ${lnk}.utf8 ${lnk}
 done
